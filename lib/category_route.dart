e9c634865ae41d1bd7455cc36a5a1bcfb45c1f74
mj <onyangomaureen95@gmail.com>
2019-03-01 21:23:14 +0300

    create category route as a statefulwidget

---
+++ lib/category_route.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CategoryRoute extends StatefulWidget{
+
+  @override
+
+  _CategoryRouteState createState()=> _CategoryRouteState();
+
+}
+
+class _CategoryRouteState extends State<CategoryRoute>{
+
+  @override
+
+  Widget build(context){
+    return BackdropFilter(
+      
+    );
+  }
+}
