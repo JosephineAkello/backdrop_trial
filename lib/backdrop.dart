07ae63595bfcc58deef4d0a4df039bba55222f3a
mj <onyangomaureen95@gmail.com>
2019-03-04 14:55:12 +0300

    add gesturedetector

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -12,6 +12,7 @@ const _BackdropPanel({
 
 final VoidCallback onTap;
 final GestureDragUpdateCallback onVerticalDragUpdate;
+final GestureDragEndCallback onVerticalDragEnd;
 final Widget title;
 final Widget child;
 }
@@ -19,6 +20,37 @@ final Widget child;
 @override
 Widget build(context){
   return Material(
+    elevation: 2.0,
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(16.0),
+      topRight: Radius.circular(16.0),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onVerticalDragUpdate:onVerticalDragUpdate,
+          onVerticalDragEnd: onVerticalDragEnd,
+          onTap: onTap,
+          child: Container(
+            height: 48.0,
+            padding: EdgeInsetsDirectional.only(start: 16.0),
+          alignment: AlignmentDirectional.centerStart,
+          child: DefaultTextStyle(
+            style: Theme.of(context).textTheme.subhead,
+          child: title,
+          ),
+          ),
+        ),
+        Divider(
+          height: 1.0,
+        ),
+        Expanded(
+          child: child,
+        ),
+      ],
+    ),
 
   );
 }
