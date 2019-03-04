3af222476397d4b27a4522cc61ecac630676108f
mj <onyangomaureen95@gmail.com>
2019-03-04 14:32:36 +0300

    create backdrop class

---
+++ lib/backdrop.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class _BackdropPanel extends StatelessWidget{
+
+}
+
+@
+Widget build(context){
+  return Material(
+
+  );
+}
+
+class _BackdropTitle extends AnimatedWidget{
+
+}
+
+@override
+
+Widget build(context){
+  return DefaultTextStyle(
+
+  );
+}
