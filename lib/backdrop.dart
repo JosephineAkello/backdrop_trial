df1c6559544eb195db998da405de3ff1db8336db
mj <onyangomaureen95@gmail.com>
2019-03-04 14:40:51 +0300

    add backdrop panel as a const and void  call back as onTap

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class _BackdropPanel extends StatelessWidget{
+const _BackdropPanel({
+  Key key,
+  this.onTap,
+  this.onVerticalDragUpdate,
+  this.onVerticalDragEnd,
+  this.title,
+  this.child,
+})  : super(key: key);
 
+final VoidCallback onTap;
+final GestureDragUpdateCallback onVerticalDragUpdate;
+final Widget title;
+final Widget child;
 }
 
-@
+@override
 Widget build(context){
   return Material(
 
