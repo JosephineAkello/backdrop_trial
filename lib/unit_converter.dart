48e7d036a7b6c7c7fef08de7d34325d76cdc8e92
mj <onyangomaureen95@gmail.com>
2019-03-07 18:23:37 +0300

    create unitconverter as a stateful widget

---
+++ lib/unit_converter.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class UnitConverter extends StatefulWidget{
+
+_UnitConverterState createState() => _UnitConverterState();
+}
+
+class _UnitConverterState extends State<UnitConverter>{
+  @override
+  Widget build(context){
+    return Padding();
+  }
+}
