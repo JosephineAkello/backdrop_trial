d6cf47dc971c7a0215f2f67a1b9e5851a4c36d60
mj <onyangomaureen95@gmail.com>
2019-03-07 18:14:29 +0300

    initialize borderradius and rowheight

--- lib/category_tile.dart
+++ lib/category_tile.dart
@@ -2,12 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'category.dart';
 
+const _rowHeight=100.0;
+final _borderRadius=BorderRadius.circular(_rowHeight /2);
+
+
 class CategoryTile extends StatelessWidget{
+final Category category;
+final ValueChanged<Category> onTap;
+
+const CategoryTile({
+  Key key,
+  @required this.category,
+  @required this.onTap,
+})  : assert(category != null),
+     assert(onTap != null),
+     super(key: key);
+
+     @override
 
   Widget build(context){
     return Material(
       color: Colors.transparent,
       child: Container(
+        height: _rowHeight,
         child: InkWell(
           borderRadius: _borderRadius,
           highlightColor: category.color['highlight'],
