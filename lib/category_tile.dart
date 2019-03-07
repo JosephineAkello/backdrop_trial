ec3e1872fe47c2cc94a7ed393f8c3c6c6e1bb85a
mj <onyangomaureen95@gmail.com>
2019-03-07 18:01:26 +0300

    add on tap function

--- lib/category_tile.dart
+++ lib/category_tile.dart
@@ -11,6 +11,30 @@ class CategoryTile extends StatelessWidget{
           borderRadius: _borderRadius,
           highlightColor: category.color['highlight'],
           splashColor: category.color['splash'],
+          
+          onTap: () => onTap(category),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Icon(
+                  category.iconLocation,
+                  size: 60.0,
+                ),
+              ),
+              Center(
+                child: Text(
+                  category.name,
+                  textAlign: TextAlign.center,
+                  style:Theme.of(context).textTheme.headline,
+                ),
+              ),
+            ],
+            ),
+          ),
           ),
         ),
 
