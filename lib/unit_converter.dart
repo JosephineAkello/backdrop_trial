b765e99ba993e7c68ad65b1b06950b3193d70b5d
mj <onyangomaureen95@gmail.com>
2019-03-07 23:05:34 +0300

    add padding properties

--- lib/unit_converter.dart
+++ lib/unit_converter.dart
@@ -9,6 +9,32 @@ _UnitConverterState createState() => _UnitConverterState();
 class _UnitConverterState extends State<UnitConverter>{
   @override
   Widget build(context){
-    return Padding();
+    final input =Padding(
+      padding: _padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        TextField(
+          style: Theme.of(context).textTheme.display1,
+          decoration: InputDecoration(
+            labelStyle: Theme.of(context).textTheme.display1,
+            errorText: _showValidationError ?'Invalid number entered' : null,
+            labelText: 'Input',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(0.0),
+            ),
+          ),
+          keyboardType: TextInputType.number,
+          onChanged: _updateInputValue,
+        ),
+        _createDropdown(_fromValue.name, _updateFromConversion),
+
+      ],
+      ),
+    );
+    return Padding(
+      padding: _padding,
+      child: converter,
+    );
   }
 }
