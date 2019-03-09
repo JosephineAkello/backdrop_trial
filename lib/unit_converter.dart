3d435a4a9abd7558248f1c3cc216cfb6862c5c30
mj <onyangomaureen95@gmail.com>
2019-03-10 00:16:28 +0300

    create Dropdown widget

--- lib/unit_converter.dart
+++ lib/unit_converter.dart
@@ -122,6 +122,36 @@ return widget.category.units.firstWhere(
         _updateConversion();
       }
  }
+
+ Widget _createDropdown(String currentValue, ValueChanged<dynamic> onChanged){
+   return Container(
+     margin: EdgeInsets.only(top: 16.0),
+     decoration: BoxDecoration(
+       color: Colors.grey[50],
+       border: Border.all(
+         color: Colors.grey[400],
+         width: 1.0,
+       ),
+     ),
+     padding: EdgeInsets.symmetric(vertical: 8.0),
+     child: Theme(
+       data: Theme.of(context).copyWith(
+         canvasColor: Colors.grey[50],
+       ),
+       child: DropdownButtonHideUnderline(
+         child: ButtonTheme(
+           alignedDropdown: true,
+           child: DropdownButton(
+             value: currentValue,
+             items: _unitMenuItems,
+             onChanged: onChanged,
+             style: Theme.of(context).textTheme.title,
+           ),
+         ),
+       ),
+     ),
+   );
+ }
   @override
   Widget build(context){
     final input =Padding(
