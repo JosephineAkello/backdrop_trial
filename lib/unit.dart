80a076ed038c34cbf260382e24ef17bc330d6767
mj <onyangomaureen95@gmail.com>
2019-03-10 01:43:24 +0300

    format document

--- lib/unit.dart
+++ lib/unit.dart
@@ -5,16 +5,15 @@ class Unit {
   final String name;
   final double conversion;
 
+  const Unit({
+    @required this.name,
+    @required this.conversion,
+  })  : assert(name != null),
+        assert(conversion != null);
 
-const Unit({
-  @required this.name,
-  @required this.conversion,
-}) : assert(name != null),
-assert(conversion != null);
-
-Unit.fromJson(Map jsonMap)
-: assert(jsonMap['name'] != null),
-assert(jsonMap['conversion'] != null),
-name = jsonMap['name'],
-conversion = jsonMap['conversion'].toDouble();
+  Unit.fromJson(Map jsonMap)
+      : assert(jsonMap['name'] != null),
+        assert(jsonMap['conversion'] != null),
+        name = jsonMap['name'],
+        conversion = jsonMap['conversion'].toDouble();
 }
