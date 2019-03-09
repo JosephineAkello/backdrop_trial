e7eadec06d5207db8e8e84492c5af51a68df5548
mj <onyangomaureen95@gmail.com>
2019-03-10 01:36:19 +0300

    format document

--- lib/category_tile.dart
+++ lib/category_tile.dart
@@ -2,25 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'category.dart';
 
-const _rowHeight=100.0;
-final _borderRadius=BorderRadius.circular(_rowHeight /2);
+const _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
 
+class CategoryTile extends StatelessWidget {
+  final Category category;
+  final ValueChanged<Category> onTap;
 
-class CategoryTile extends StatelessWidget{
-final Category category;
-final ValueChanged<Category> onTap;
+  const CategoryTile({
+    Key key,
+    @required this.category,
+    @required this.onTap,
+  })  : assert(category != null),
+        assert(onTap != null),
+        super(key: key);
 
-const CategoryTile({
-  Key key,
-  @required this.category,
-  @required this.onTap,
-})  : assert(category != null),
-     assert(onTap != null),
-     super(key: key);
-
-     @override
-
-  Widget build(context){
+  @override
+  Widget build(context) {
     return Material(
       color: Colors.transparent,
       child: Container(
@@ -29,33 +27,31 @@ const CategoryTile({
           borderRadius: _borderRadius,
           highlightColor: category.color['highlight'],
           splashColor: category.color['splash'],
-          
           onTap: () => onTap(category),
           child: Padding(
             padding: EdgeInsets.all(8.0),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Icon(
-                  category.iconLocation,
-                  size: 60.0,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Icon(
+                    category.iconLocation,
+                    size: 60.0,
+                  ),
                 ),
-              ),
-              Center(
-                child: Text(
-                  category.name,
-                  textAlign: TextAlign.center,
-                  style:Theme.of(context).textTheme.headline,
+                Center(
+                  child: Text(
+                    category.name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline,
+                  ),
                 ),
-              ),
-            ],
+              ],
             ),
           ),
-          ),
         ),
-
-      );
+      ),
+    );
   }
 }
