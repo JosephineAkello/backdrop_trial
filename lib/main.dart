f016f6d55df6d97fe0bedad952952e5140bfc439
mj <onyangomaureen95@gmail.com>
2019-03-10 01:41:13 +0300

    format document

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 import 'category_route.dart';
 
-void main(){
+void main() {
   runApp(UnitConverterApp());
 }
 
-class UnitConverterApp extends StatelessWidget{
+class UnitConverterApp extends StatelessWidget {
   @override
-
-  Widget build(context){
+  Widget build(context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
       theme: ThemeData(
         textTheme: Theme.of(context).textTheme.apply(
-          bodyColor: Colors.black,
-          displayColor: Colors.grey[600],
-        ),
+              bodyColor: Colors.black,
+              displayColor: Colors.grey[600],
+            ),
         primaryColor: Colors.grey[500],
         textSelectionHandleColor: Colors.green[500],
       ),
